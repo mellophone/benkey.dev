5858fc4a87be5524cd382a0cbffd8fd812d09efd
Ben Key <54153434+mellophone@users.noreply.github.com>
2024-03-29 11:44:04 -0500

    Black apple bar test

--- src/components/KeyBitCanvas/index.tsx
+++ src/components/KeyBitCanvas/index.tsx
@@ -34,10 +34,7 @@ const KeyBitCanvas = (props: { mapObject: MapObject }) => {
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="theme-color" content="#000000" />
       </Head>
       <canvas
