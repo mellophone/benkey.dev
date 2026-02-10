5a0a521491595b316540b3541a5c6a575cba8e3f
Ben Key <54153434+mellophone@users.noreply.github.com>
2026-02-10 11:41:45 -0600

    Add WIP page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,60 +5,30 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className={styles.fullwidth}>
+          <div className={styles.hstack}>
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              alt="Ben's Face"
+              src="/ben-head-outline.png"
+              width={200}
+              height={200}
+              style={{ borderRadius: "100%" }}
             />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <div className={styles.hammer}>🔨</div>
+          </div>
+          <div className={styles.hstack}>
+            <h1>Ben Key</h1>
+          </div>
+          <p>
+            {`Software developer and problem solver currently based in North 
+            Texas. Looking for exciting new opportunities! You can reach me at `}
+            <a href="mailto:bkey3125@gmail.com">bkey3125@gmail.com</a>.
+          </p>
+          <br />
+          <p>
+            Recreating this site from scratch right now, so it's a
+            work-in-progress! Check back for frequent updates. 😁
+          </p>
         </div>
       </main>
     </div>
